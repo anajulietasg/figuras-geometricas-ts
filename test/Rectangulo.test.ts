74b43c56a9303463bd5fd295e4042945450f1a23
Ana Julieta Sanchez <anajulietasg@gmail.com>
2026-09-10 12:37:56 -0300

    corrijo pruebas unitarias de Rectangulo

--- test/Rectangulo.test.ts
+++ test/Rectangulo.test.ts
@@ -1,19 +1,19 @@
 import { describe, expect, test } from "vitest";
-import { Cuadrado } from "../src/Cuadrado";
+import { Rectangulo } from "../src/Rectangulo";
 
 describe("Rectangulo", () => {
     test("se puede crear un rectangulo", () => {
-        const rectangulo = new Cuadrado(5);
+        const rectangulo = new Rectangulo(5, 3);
         expect(rectangulo).not.toBeNull();
     });
 
     test("el area de un rectangulo es correcto", () => {
-        const rectangulo = new Cuadrado(5);
-        expect(rectangulo.calcularArea()).toBe(25);
+        const rectangulo = new Rectangulo(5, 3);
+        expect(rectangulo.calcularArea()).toBe(15);
     });
 
     test("el perimetro de un rectangulo es correcto", () => {
-        const rectangulo = new Cuadrado(5);
-        expect(rectangulo.calcularPerimetro()).toBe(20);
+        const rectangulo = new Rectangulo(5, 3);
+        expect(rectangulo.calcularPerimetro()).toBe(16);
     });
 });
